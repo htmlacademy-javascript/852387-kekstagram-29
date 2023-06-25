de88b9607b2a320ffca56c2b58c1cbf343625226
tankov <domino-82@mail.ru>
2023-06-25 22:43:24 +0300

    добавляет функцию extractNumber

--- js/functions.js
+++ js/functions.js
@@ -3,6 +3,12 @@ function checkStringLength (str, length) {
   return str.length <= length;
 }
 
+checkStringLength('проверяемая строка', 20); // true
+// Длина строки ровно 18 символов
+checkStringLength('проверяемая строка', 18); // true
+// Строка длиннее 10 символов
+checkStringLength('проверяемая строка', 10); // false
+
 // Функция для проверки, является ли строка палиндромом.
 function checkStringPolyndrom (str) {
   const newStr = str.replaceAll(' ', '').toLowerCase();
@@ -12,3 +18,32 @@ function checkStringPolyndrom (str) {
   }
   return result === newStr;
 }
+
+// Строка является палиндромом
+checkStringPolyndrom('топот'); // true
+// Несмотря на разный регистр, тоже палиндром
+checkStringPolyndrom('ДовОд'); // true
+// Это не палиндром
+checkStringPolyndrom('Кекс'); // false
+// Это палиндром
+checkStringPolyndrom('Лёша на полке клопа нашёл '); // true
+
+// Функция извлекает число из строки
+function extractNumber (chars) {
+  const str = chars.toString();
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    const parsed = parseInt(str[i], 10);
+    result += !isNaN(parsed) ? str[i] : '';
+  }
+  return parseInt(result, 10);
+}
+
+extractNumber('2023 год'); // 2023
+extractNumber('ECMAScript 2022'); // 2022
+extractNumber('1 кефир, 0.5 батона'); // 105
+extractNumber('агент 007'); // 7
+extractNumber('а я томат'); // NaN
+extractNumber(2023); // 2023
+extractNumber(-1); // 1
+extractNumber(1.5); // 15
